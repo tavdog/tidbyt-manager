c303f80de7b302011aae777771920817550bf394
Tavis <github@tavis.la>
2023-06-17 22:36:51 -0700

    change user configs dir

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -23,6 +23,7 @@ def register():
             user = dict()
             user["username"] = username
             user["password"] = password
+            db.create_user_dir(user)
             db.save_user(user)
             return redirect(url_for('auth.login'))
         flash(error)
